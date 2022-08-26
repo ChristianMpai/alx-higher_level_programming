81851510337bdeef34667a5ccd8feff9116effb5
ChristianMpai <105034854+ChristianMpai@users.noreply.github.com>
2022-08-26 02:04:22 +0200

    function def magic_calculation(a, b)

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -3,8 +3,8 @@ def magic_calculation(a, b):
     from magic_calculation_102 import add, sub
     if a < b:
         c = add(a, b)
-        for i in ramge(4, 6):
+        for i in range(4, 6):
             c = add(c, i)
-            return (c)
-        else:
-            return (sub(a, b))
+        return (c)
+    else:
+        return (sub(a, b))
