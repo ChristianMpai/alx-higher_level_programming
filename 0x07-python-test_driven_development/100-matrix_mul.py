fa5dde610b3df29d72231c233af427473058c49c
ChristianMpai <105034854+ChristianMpai@users.noreply.github.com>
2022-09-21 02:14:30 +0200

    function that multiplies 2 matrices

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
 """
+
 Module composed by a function that multiplies 2 matrices
+
 """
 
 
 def matrix_mul(m_a, m_b):
     """ Function that multiplies 2 matrices
+    
     Args:
         m_a: matrix a
         m_b: matrix b
+        
     Returns:
         result of the multiplication
+        
     Raises:
         TypeError: if m_a or m_b aren't a list
         TypeError: if m_a or m_b aren't a list of a lists
@@ -18,6 +23,8 @@ def matrix_mul(m_a, m_b):
         TypeError: if the lists of m_a or m_b don't have integers or floats
         TypeError: if the rows of m_a or m_b don't have the same size
         ValueError: if m_a and m_b can't be multiplied
+        
+        
     """
 
     if not isinstance(m_a, list):
