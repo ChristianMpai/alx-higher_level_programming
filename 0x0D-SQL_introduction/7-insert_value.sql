b4f5b5b8c820a39f70745f55da791e73cab19b12
ChristianMpai <tebogotebzana@gmail.com>
2022-11-01 06:18:54 -0700

    inserts a new row in the table first_table

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,2 @@
--- Inserts a new row in a table
--- Query to insert a new row in the table first_table
-INSERT INTO first_table (id, name) VALUES (89, "name = Best School");
+-- insert values at sql table
+INSERT INTO first_table (id, name) VALUES (89, "Best School");
