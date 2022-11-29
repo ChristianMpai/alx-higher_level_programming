c128c9ceaaa4322fbeb04ab4104f031325a6cd46
ChristianMpai <105034854+ChristianMpai@users.noreply.github.com>
2022-11-29 17:33:00 +0200

    3-rectangle.js

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -11,7 +11,7 @@ class Rectangle {
     for (let i = 0; i < this.height; i++) {
       let s = '';
       for (let j = 0; j < this.width; j++) {
-	s += 'X';
+        s += 'X';
       }
       console.log(s);
     }
